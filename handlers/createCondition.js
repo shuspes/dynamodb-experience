a1fddeeed8b6daecdee8a40a5c6a451f8b5b7c5c
Alexander Spesivtsev <Alexander_Spesivtsev@epam.com>
2018-08-06 17:48:31 +0300

    (work) add dynogels to project

--- handlers/createCondition.js
+++ handlers/createCondition.js
@@ -1,39 +1,85 @@
 const uuid = require('uuid');
 const { dynamoDb, TABLE_NAME } = require('../utils/constants');
 
+const dynogels = require('dynogels');
+const Joi = require('joi');
+
+const Item = dynogels.define('Item', {
+    hashKey : 'ID', 
+    rangeKey : 'Title', 
+    schema : {
+      ID: dynogels.types.uuid(),
+      Title: Joi.string(),
+      Date: Joi.string(),
+      Desciption: Joi.string(),
+      Count: Joi.number(),
+      Number: Joi.number()
+    },
+    tableName: TABLE_NAME
+  });
+
 module.exports.createCondition = (event, context, callback) => {
     const timestamp = new Date().getTime();
     const data = JSON.parse(event.body);
 
-    const test_func = (test_func, test2, test3, test4) => {
-        console.log("adfadfadfadf !!!!!!!!!!!!!!!!");
-        return true;
-        
-    }
-  
-    const params = {
-      TableName: TABLE_NAME,
-      Item: {
+
+    let params = {};
+    params.ConditionExpression = 'attribute_not_exists(#num)';
+    params.ExpressionAttributeNames = {'#num' : 'Number'};
+    // params.ExpressionAttributeValues = {':x' : 123};
+
+    const item = {
         ID: data.ID || uuid.v1(),
         Title: data.Title || '',
-        Date: timestamp,
+        Date: timestamp.toString(),
         Desciption: data.Desciption || '',
         Count: data.Count || 0,
         Number: data.Number
-      },
-      ExpressionAttributeNames: {"#num": "Number"},
-    //   ExpressionAttributeValues: {":number": "2"},
-      ConditionExpression: "attribute_not_exists(#num)"
-    };
+    }
+  
+    Item.create(item, params, function (error, acc) {
+        let result = ''
+        if(error) {
+            result = error;
+        } else {
+            result = acc;
+        }
+
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(result),
+        };
+        callback(null, response);
+    });
+
+
+
+
+
+  
+    // const params = {
+    //   TableName: TABLE_NAME,
+    //   Item: {
+    //     ID: data.ID || uuid.v1(),
+    //     Title: data.Title || '',
+    //     Date: timestamp,
+    //     Desciption: data.Desciption || '',
+    //     Count: data.Count || 0,
+    //     Number: data.Number
+    //   },
+    //   ExpressionAttributeNames: {"#num": "Number"},
+    // //   ExpressionAttributeValues: {":number": "2"},
+    //   ConditionExpression: "attribute_not_exists(#num)"
+    // };
     
-    dynamoDb.put(params).promise()
-        .then(data => data)
-        .catch(err => err)
-        .then(responseMessage => {
-            const response = {
-                statusCode: 200,
-                body: JSON.stringify(responseMessage),
-            };
-            callback(null, response);
-        });
+    // dynamoDb.put(params).promise()
+    //     .then(data => data)
+    //     .catch(err => err)
+    //     .then(responseMessage => {
+    //         const response = {
+    //             statusCode: 200,
+    //             body: JSON.stringify(responseMessage),
+    //         };
+    //         callback(null, response);
+    //     });
   };
